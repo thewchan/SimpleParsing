340105dbf55318e86775aa213e9d77d47ee00337
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-08-18 23:49:58 -0400

    Better illustrate the 'sub-entry' idea of issue30
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- test/utils/test_issue_30.py
+++ test/utils/test_issue_30.py
@@ -10,10 +10,39 @@ class NetworkOptions(Serializable):
     input_dimensions: Tuple[int, int]
 
 
+@dataclass
+class TrainingScriptOptions(Serializable):
+    learning_rate: float
+    network_options: NetworkOptions
+
+
 def test_issue_30(tmpdir):
-    network = NetworkOptions(num_layers=10, output_dimension=4, input_dimensions=(16, 16))
-    print(network.to_dict())
-    network.save(f"{tmpdir}/train-settings.yaml")
-    network_ = NetworkOptions.load_yaml(f"{tmpdir}/train-settings.yaml")
+    network = NetworkOptions(
+        num_layers=10,
+        output_dimension=4,
+        input_dimensions=(16, 16),
+    )
+    training_options = TrainingScriptOptions(
+        learning_rate=3e-4,
+        network_options=network,
+    )
+
+    path: str = f"{tmpdir}/train-settings.yaml"
+
+    # Save the 'bigger' object somewhere:
+    training_options.save(path)
+    
+    # You're right, you either have to:
+    # 1. Load the parent object and then get the right attribute:
+    training_options_ = TrainingScriptOptions.load_yaml(path)
+    network_ = training_options_.network_options
+    assert network == network_
+
+    # OR:
+    # 2. Load the yaml file manually and parse the child from the dict entry:
+    import yaml
+    with open(f"{tmpdir}/train-settings.yaml") as f:
+        train_options_dict = yaml.safe_load(f)
+    network_ = NetworkOptions.from_dict(train_options_dict["network_options"])
 
     assert network == network_
